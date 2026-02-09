97970bd4dedb4bb61da96f29e33c4a99d56e6197
wbr5094 <wbr5094@psu.edu>
2026-02-09 10:52:59 -0500

    change card name

--- src/my-card.js
+++ src/my-card.js
@@ -19,6 +19,52 @@ export class MyCard extends LitElement {
 
   static get styles() {
     return css`
+    .card.fancy {
+  background-color: red;
+}
+
+h1 {
+  font-size: 10px;
+}
+
+#card-list {
+  display: flex;
+}
+.card {
+  font-size: 1em;
+  display: inline-flex;
+  border: 2px solid grey;
+  padding: 8px;
+  margin: 8px;
+  opacity: .8;
+  background-color: navy;
+}
+
+.card-image {
+  width: 200px;
+  height: 100%;
+}
+
+.card-text {
+  width: 300px;
+  padding: 0 8px 8px 8px;
+  color: black;
+  background-color: white;
+  margin: 0 0 0 8px;
+  height: 300px;
+  overflow: auto;
+}
+
+.card-title {
+  position: sticky;
+  top: 0;
+  background-color: var(--my-card-title-background-color, #eeeeee);
+  color: var(--my-card-title-color, black);
+  text-align: center;
+  font-size: 2em;
+  padding: 8px 8px 16px;
+  margin: 0 -8px;
+}
       :host {
         display: block;
       }
@@ -26,12 +72,24 @@ export class MyCard extends LitElement {
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div class="control-wrapper">
+</div>
+<div id="cardlist">
+  <div class="card">
+    <img class="card-image" ${this.image}src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4431678.png&w=350&h=254" />
+    <div class="card-text">
+      <h1 class="card-title">${this.title}</h1>
+      <div class="card-details">
+        <div class="card-details">
+        <p>
+        Tyrese Maxey is the best basketball player in the world. <p></p>`;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: {type: String},
+      alt: {type: String},
     };
   }
 }
--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -29,6 +29,7 @@ export class PolarisChip extends LitElement {
     super();
     // a variable on this object called title
     this.title = 'Chip Default';
+    this.link = 'https://example.com';
   }
 
   // CSS styles are scoped JUST to this element. This uses a technology called
@@ -49,7 +50,7 @@ export class PolarisChip extends LitElement {
         display: inline-flex;
       }
 
-      span {
+      a {
         background-color: orange;
         color: black;
         font-size: 24px;
@@ -57,7 +58,8 @@ export class PolarisChip extends LitElement {
         margin: 8px;
       }
 
-      span:hover {
+      a:focus-within,
+      a:hover {
         background-color: grey;
         border: 1px solid black;
       }
@@ -83,7 +85,12 @@ export class PolarisChip extends LitElement {
     // it is going to print the title of the element. The magic of Lit is that
     // when title is changed (even by inspecting the document and hacking the value)
     // it will automatically update what is displayed and do so incredibly quickly
-    return html`<span>${this.title}</span>`;
+    return html`
+    <span>
+    <a href="${this.link}">
+    ${this.title}
+    </a>
+    </span>`;
   }
 
   // LitElement uses the properties call to do the following:
@@ -95,6 +102,7 @@ export class PolarisChip extends LitElement {
     return {
       // this is a String. Array, Object, Number, Boolean are other valid values here
       title: { type: String },
+      link: {type: String },
     };
   }
 }
