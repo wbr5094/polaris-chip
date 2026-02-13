daa1ffe629a044566c8424c489bd0da0768417a3
wbr5094 <wbr5094@psu.edu>
2026-02-13 10:14:48 -0500

    my card 3

--- src/my-card.js
+++ src/my-card.js
@@ -17,7 +17,8 @@ export class MyCard extends LitElement {
       title: { type: String },
       image: {type: String},
       alt: {type: String},
-      description: {type: String}
+      description: {type: String},
+      fancy: { type: Boolean, reflect: true }
     };
   }
 
@@ -27,6 +28,7 @@ export class MyCard extends LitElement {
     this.image = '';
     this.alt = '';
     this.description = '';
+    this.fancy = false;
   }
 
   static get styles() {
@@ -34,6 +36,15 @@ export class MyCard extends LitElement {
     :host {
       display: inline-block;
     }
+
+    :host([fancy]) {
+display: inline-block;
+  background-color: pink;
+  border: 2px solid fuchsia;
+  box-shadow: 10px 5px 5px red;
+  width: 550px;
+}
+
     
 .card {
   font-size: 1em;
@@ -72,6 +83,16 @@ export class MyCard extends LitElement {
     `;
   }
 
+  openChanged(e) {
+  console.log(e);
+  if (e.target.getAttribute('open') !== null) {
+    this.fancy = true;
+  }
+  else {
+    this.fancy = false;
+  }
+}
+
   render() {
     return html`
     <div class ="card">
@@ -82,11 +103,18 @@ export class MyCard extends LitElement {
       />
       <div class="card-text">
         <h1 class="card-title">${this.title} </h1>
-        <p>${this.description}</p>
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+  <summary>Description</summary>
+  <div>
+    <slot>${this.description}</slot>
+  </div>
+ </details>
+
       </div>
     </div>  
   `;
   }
+
 }
 
 globalThis.customElements.define(MyCard.tag, MyCard);
